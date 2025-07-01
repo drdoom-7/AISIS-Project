61e88f6ce0cfbcdfd87d27038f1c2200cda5ecbb
drdoom-7 <yourdoom666999@gmail.com>
2025-07-01 08:35:09 +0000

    perfect-justincasecopy

--- instruments/default/bing_image_downloader/bing_image_downloader.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Get the directory of the current script
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-
-# Execute the main Python script
-python3 "$SCRIPT_DIR/downloader.py" "$@"
